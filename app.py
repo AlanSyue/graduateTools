7d774c6b51557859f9d88d6cc15aca17faf9b1b8
alansyue <b123105@gmail.com>
2019-11-18 21:31:55 +0800

    upload

---
+++ app.py
@@ -0,0 +1,194 @@
+#!flask/bin/python
+from flask import Flask
+from flask import request
+from flask import jsonify
+from flask import Response
+import requests
+from bs4 import BeautifulSoup
+import json
+
+def get_hcm_credit():
+	with open("hcm.txt") as file:
+		data = json.load(file)
+	return data['hcm']
+
+def get_general_credit():
+	with open("general.txt") as file:
+		data = json.load(file)
+	return data['general']
+
+app = Flask(__name__)
+
+@app.route('/api/count', methods=['POST'])
+def create_task():
+	data = request.get_json()
+
+	subjectCodeArray = []
+
+	domain_list = [
+		"https://ep.cgu.edu.tw/webfolio/progression_1.aspx?RC_Level=1&RC_Semester=U",
+		"https://ep.cgu.edu.tw/webfolio/progression_1.aspx?RC_Level=1&RC_Semester=D",
+		"https://ep.cgu.edu.tw/webfolio/progression_1.aspx?RC_Level=2&RC_Semester=U",
+		"https://ep.cgu.edu.tw/webfolio/progression_1.aspx?RC_Level=2&RC_Semester=D",
+		"https://ep.cgu.edu.tw/webfolio/progression_1.aspx?RC_Level=2&RC_Semester=S",
+		"https://ep.cgu.edu.tw/webfolio/progression_1.aspx?RC_Level=3&RC_Semester=U",
+		"https://ep.cgu.edu.tw/webfolio/progression_1.aspx?RC_Level=3&RC_Semester=D",
+		"https://ep.cgu.edu.tw/webfolio/progression_1.aspx?RC_Level=3&RC_Semester=S",
+		"https://ep.cgu.edu.tw/webfolio/progression_1.aspx?RC_Level=4&RC_Semester=U",
+		"https://ep.cgu.edu.tw/webfolio/progression_1.aspx?RC_Level=4&RC_Semester=D",
+	]
+
+	for domain in domain_list:
+		#將資料加入 POST 請求中
+		res = requests.post(domain, cookies = {'Cookie':data['Cookie']})
+		bs = BeautifulSoup(res.text,'html.parser')
+		h1 = bs.find('h1').text
+		trs = bs.findAll('tr')
+		for row in trs:
+			code = row.text.split("\n")[1]
+			if code.isnumeric():
+				score = row.text.split("\n")[4]
+				if score.isnumeric():
+					if int(score) >= 60:
+						subjectCodeArray.append(code)
+	print("================================================")
+	count_hcm_credit(subjectCodeArray)
+	print("================================================")
+	result = '123'
+	return Response(result, mimetype='text/plain')
+
+def count_hcm_credit(subjectCodes):
+	hcmCredit = get_hcm_credit()
+	hcmCreditData = {}
+
+	haveToTotal = 0
+	majorTotal = 0
+	optionTotal = 0
+	fourChooseTwoCount = 0
+	fourChooseTwoTotal = 0
+
+	generalSubjectCredit = []
+
+	for credit in hcmCredit:
+		hcmCreditData.update(credit)
+
+	for mySubjectCode in subjectCodes:
+		if mySubjectCode in hcmCreditData:
+			subjectType = hcmCreditData[mySubjectCode][0]['type']
+			subjectCredit = int(hcmCreditData[mySubjectCode][0]['credit'])
+			if subjectType == '0':
+				haveToTotal+=1
+			elif subjectType == '1':
+				majorTotal += subjectCredit
+			elif subjectType == '2':
+				optionTotal += subjectCredit
+			elif subjectType == '3':
+				fourChooseTwoCount +=0
+		else:
+			generalSubjectCredit.append(mySubjectCode)
+
+	if haveToTotal < 7:
+		print("軍訓、體育、社會關懷實作尚有未完成")
+	if majorTotal < 76:
+		print("必修尚缺" + str(majorTotal - 76) + " 學分")
+	if optionTotal < 12:
+		print("選修尚缺 " + str(optionTotal - 12) + " 學分")
+	if fourChooseTwoCount < 2:
+		print("四選二尚缺 " + str(2 - fourChooseTwoCount) + " 門")
+
+	count_general_credit(generalSubjectCredit)
+
+def count_general_credit(generalSubjectCredit):
+	generalCredit = get_general_credit()
+	generalCreditData = {}
+
+	for credit in generalCredit:
+		generalCreditData.update(credit)
+
+	for mySubjectCode in generalSubjectCredit:
+		if mySubjectCode in generalCreditData:
+
+			subjectType = generalCreditData[mySubjectCode][0]['type']
+			try:
+				subjectCategory = generalCreditData[mySubjectCode][0]['category']
+			except:
+				pass
+			subjectCredit = int(generalCreditData[mySubjectCode][0]['credit'])
+
+			type1SocialCount = 0
+			type1SocialTotal = 0
+
+			type1ArtCount = 0
+			type1ArtTotal = 0
+
+			type2ArtCount = 0
+			type2ArtTotal = 0
+
+			type2HistoryCount = 0
+			type2HistoryTotal = 0
+
+			type2SocialScienceCount = 0
+			type2SocialScienceTotal = 0
+
+			type2BioScienceCount = 0
+			type2BioScienceTotal = 0
+
+			type2MathCount = 0
+			type2MathTotal = 0
+
+			type3EngCount = 0
+			otherTotal = 0
+
+			if subjectType == '1':
+				if subjectCategory == '社會':
+					type1SocialCount += 1
+					type1SocialTotal += subjectCredit
+				elif subjectCategory == '人文藝術':
+					type1ArtCount += 1
+					type1ArtTotal += subjectCredit	
+			elif subjectType == '2':
+				if subjectCategory == '文學與藝術':
+					type2ArtCount += 1
+					type2ArtTotal += subjectCredit
+				elif subjectCategory == '哲學與歷史':
+					type2HistoryCount += 1
+					type2HistoryTotal += subjectCredit
+				elif subjectCategory == '社會科學':
+					type2SocialScienceCount += 1
+					type2SocialScienceTotal += subjectCredit
+				elif subjectCategory == '生命科學':
+					type2BioScienceCount += 1
+					type2BioScienceTotal += subjectCredit
+				elif subjectCategory == '物質科學與數理邏輯':
+					type2MathCount += 1
+					type2MathTotal += subjectCredit
+			elif subjectType == '3':
+				if subjectCategory == '英文':
+					type3EngCount += subjectCredit
+			else:
+				otherTotal += subjectCredit
+
+	if type1SocialTotal < 6:
+		print("通識-社會科學核心課程尚缺 " + str(6-type1SocialTotal) + " 學分")
+	if type1ArtTotal < 6:
+		print("通識-人文藝術核心課程尚缺 " + str(6-type1SocialTotal) + " 學分")
+	if type3EngCount < 6:
+		print("通識-英文領域尚缺 " + str(6-type3EngCount) + " 學分")
+
+	checkType2List = [
+		type2ArtCount, type2HistoryCount, type2SocialScienceCount,
+		type2BioScienceCount, type2MathCount
+	]
+
+	checkRun = 0
+	for type2Count in checkType2List:
+		if type2Count == 0:
+			checkRun+=1
+	if checkRun >=3:
+		print("通識-多元課程尚缺" + str(3 - ( 5- checkRun )) + "個領域（至少選修3領域)")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
