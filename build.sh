294590bebae259a07bd3f780d5bd212a54e58436
薛翔文 <alansyue@xiesonghuade-MacBook-Pro-2.local>
2020-01-21 17:43:11 +0800

    docker connect local file with container file

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ NC='\033[0m' # no color
 printf "========== ${RED} Start build docker image ${NC} ==========\n"
 docker build -t graduatetool . --no-cache
 printf "========== ${RED} Build docker image successfully ${NC} ==========\n"
-docker run -d --name graduateApi -p 9000:9000 graduatetool:latest
+docker run -v $PWD:/app  -d --name graduateApi -p 9000:9000 graduatetool:latest
 printf "${RED}=====================================================${NC}\n"
 printf "========== you can connect api url with ${RED} http://localhost:9000 ${NC} ==========\n"
